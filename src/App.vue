71819de723b718a470f41f349c40109bdce93813
Husan <102412893+hwandev12@users.noreply.github.com>
2022-06-07 19:19:44 +0500

    data in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Main</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+   <h1>{{ key }}</h1>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: 'This is a home page here'
+    }
+  }
+}
+</script>
 
 <style>
 #app {
